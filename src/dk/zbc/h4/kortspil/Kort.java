f481bb892376e6e18efe06663eec3f93446aef64
Niklas Thomsen <nooblas@hotmail.com>
2015-05-05 08:55:21 +0200

    Intital codes
    All the code written 04-05-2015

---
+++ src/dk/zbc/h4/kortspil/Kort.java
@@ -0,0 +1,34 @@
+package dk.zbc.h4.kortspil;
+
+/**
+ * Created by Niklas on 04-05-2015.
+ */
+public class Kort {
+    public int getVaerdi() {
+        return vaerdi;
+    }
+
+    public void setVaerdi(int vaerdi) {
+        this.vaerdi = vaerdi;
+    }
+
+    public Kuloer getKuloer() {
+        return kuloer;
+    }
+
+    public void setKuloer(Kuloer kuloer) {
+        this.kuloer = kuloer;
+    }
+
+    private static enum Kuloer {
+        KLOER, HJERTER, SPAR, RUDER, JOKER
+    }
+    private int vaerdi;
+    private Kuloer kuloer;
+
+    public Kort(int vaerdi, Kuloer kuloer)
+    {
+        this.setKuloer(kuloer);
+        this.setVaerdi(vaerdi);
+    }
+}
