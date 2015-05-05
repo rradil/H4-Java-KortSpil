65213cf9f7ca25aa08d8df4226bfcbc88344ccb6
Patrick Nærvig Wøhlk <patrick@webbanditten.dk>
2015-05-05 13:28:29 +0200

    Lavet bland funktion

--- src/dk/zbc/h4/kortspil/SorteperMgr.java
+++ src/dk/zbc/h4/kortspil/SorteperMgr.java
@@ -5,12 +5,36 @@
  */
 package dk.zbc.h4.kortspil;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author runra
  */
 public class SorteperMgr extends KortspilMgr {
-    public void startSpil() {};
+
+
+
+    public void startSpil() {
+
+        Deck dk = new Deck();
+        for (int i = 0; i <= 13; i++) {
+            for(int kuloer = 0; kuloer <= 4; kuloer++) {
+
+                if(kuloer == 1) {
+                    dk.tilfoejKort(new Kort(i, Kort.Kuloer.HJERTER));
+                }else if(kuloer == 2) {
+                    dk.tilfoejKort(new Kort(i, Kort.Kuloer.KLOER));
+                }else if(kuloer == 3) {
+                    dk.tilfoejKort(new Kort(i, Kort.Kuloer.RUDER));
+                }else if(kuloer == 4) {
+                    dk.tilfoejKort(new Kort(i, Kort.Kuloer.SPAR));
+                }
+            }
+
+        }
+        dk.tilfoejKort(new Kort(15, Kort.Kuloer.JOKER));
+    };
     public void slutSpil() {};
     
 }
