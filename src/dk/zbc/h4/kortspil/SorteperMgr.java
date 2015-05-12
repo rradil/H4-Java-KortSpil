2be5dbe95cb0168d953448ae8add25bf1bb3b470
Danrs4114 <Danrs4114@users.noreply.github.com>
2015-05-12 12:30:20 +0200

    ...

--- src/dk/zbc/h4/kortspil/SorteperMgr.java
+++ src/dk/zbc/h4/kortspil/SorteperMgr.java
@@ -1,6 +1,5 @@
 package dk.zbc.h4.kortspil;
 import sun.security.provider.ConfigFile;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -18,29 +17,27 @@ public class SorteperMgr extends KortspilMgr {
     private SorteperMgr() {
         spillerListe = new ArrayList<Spiller>();
     }
-
+    
     public static SorteperMgr getInstance() {
         if(instance == null) {
             instance = new SorteperMgr();
         }
         return instance;
     }
-
+    
     
     public void startSpil() {
-
+        
         spillerListe.add(new Spiller("Niklas", "1"));
         spillerListe.add(new Spiller("Daniel", "2"));
         spillerListe.add(new Spiller("Patrick", "3"));
-        spillerListe.add(new Spiller("Louise", "4"));
-
-
-
+        spillerListe.add(new Spiller("Louise", "4"));      
+        
         Deck dk = new Deck();
         for (int i = 1; i <= 13; i++) {
             for(int kuloer = 1; kuloer <= 4; kuloer++) {
                 
-
+                
                 if(kuloer == 1) {
                     dk.tilfoejKort(new Kort(i, Kort.Kuloer.HJERTER));
                 }else if(kuloer == 2) {
@@ -51,17 +48,17 @@ public class SorteperMgr extends KortspilMgr {
                     dk.tilfoejKort(new Kort(i, Kort.Kuloer.SPAR));
                 }
             }
-
+            
         }
         dk.tilfoejKort(new Kort(15, Kort.Kuloer.JOKER));
-
-                // Print, shuffle, print
+        
+        // Shuffle
         long seed = System.nanoTime();
         Collections.shuffle(dk, new Random(seed));
-        System.err.println(dk.size());
-
+        //System.err.println(dk.size());
+        
         int intCounter = 0;
-
+        
         for (int i = 0; i< dk.size(); i++){
             //Uddeler kort..
             for(Spiller s : spillerListe){
@@ -76,12 +73,12 @@ public class SorteperMgr extends KortspilMgr {
             //dk.remove(i);
         }
         dk.clear();
-
-
-            }
-
+        
+        
+    }
+    
     public void slutSpil() {};
-
+    
     public Spiller getSpiller(String sessionId) {
         Spiller returSpiller = null;
         for(Spiller s : spillerListe)
@@ -93,18 +90,103 @@ public class SorteperMgr extends KortspilMgr {
         }
         return returSpiller;
     }
-
+    
     public Spiller nuvaerendeSpiller() {
         return spillerListe.get(0);
     }
-
+    
     public Spiller naesteSpiller() {
         return spillerListe.get(1);
     }
     public ArrayList<Spiller> flytSpillere() {
         return null;
     }
-
+    
+    /** Get playerList index number of previous player with cards
+     * @param currentPlayer
+     * @return Returns index number of previous player. Returns null if parameter is null.
+     */
+    public int getPreviousPlayerIndex(Spiller currentPlayer) {
+        // Check if player isn't null
+        if (currentPlayer != null) {
+            int idx = spillerListe.indexOf(currentPlayer);
+            int prevIdx = idx;
+            boolean previousPlayerValid = false;
+            
+            while (previousPlayerValid == false)
+            {
+                // Get index of previous player
+                if (idx == 0)
+                    prevIdx = spillerListe.size() - 1;
+                else
+                    prevIdx = idx - 1;
+                
+                Spiller previousPlayer = spillerListe.get(prevIdx);
+                
+                // Check if player got cards
+                if (previousPlayer.getHaand().size() != 0)
+                    previousPlayerValid = true;
+                else
+                    idx--;
+            }
+            
+            return prevIdx;
+        }
+        // Return error value -1 (No currentPlayer found)
+        return -1;
+    }
+    
+    /** Get playerList index number of next player with cards
+     * @param currentPlayer
+     * @return Returns index number of next player. Returns null if parameter is null.
+     */
+    public int getNextPlayerIndex(Spiller currentPlayer) {
+        // Check if player isn't null
+        if (currentPlayer != null) {
+            int idx = spillerListe.indexOf(currentPlayer);
+            int nextIdx = idx;
+            boolean nextPlayerValid = false;
+            
+            while (nextPlayerValid == false)
+            {
+                // Get index of next player
+                if (idx == spillerListe.size() -1)
+                    nextIdx = 0;
+                else
+                    nextIdx = idx + 1;
+                
+                Spiller nextPlayer = spillerListe.get(nextIdx);
+                
+                // Check if player got cards
+                if (nextPlayer.getHaand().size() != 0)
+                    nextPlayerValid = true;
+                else
+                    idx++;
+            }
+            
+            return nextIdx;
+        }
+        
+        // Return error value -1 (No currentPlayer found)
+        return -1;
+    }
+    
+    /** Get player by playerList index number
+     * @param index Index number of player
+     * @return Returns player. Returns null if parameter is outside the bounds of the playerlist.
+     */
+    public Spiller getPlayerByIndex(int index) {
+        // Check if index is between 0 and playerList count
+        if ((index >= 0) && (index <= (spillerListe.size() - 1))) {
+            // Get player by index
+            Spiller player = spillerListe.get(index);       
+            return player;
+        }
+        
+        // Return null if index is outside playerList bounds.
+        return null;
+    }
+    
     public static void main(String[] args) {
         SorteperMgr.getInstance().startSpil();
     }
